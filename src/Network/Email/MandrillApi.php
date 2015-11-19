8ca00e0b972204f7bb0f51f83f7950f88b24b10f
orken <subotm@free.fr>
2015-11-19 17:52:13 +0100

    ça avance

--- src/Network/Email/MandrillApi.php
+++ src/Network/Email/MandrillApi.php
@@ -12,14 +12,105 @@
 
 namespace MandrillApi\Network\Email;
 
+use Cake\Core\Configure;
+use Cake\Utility\Hash;
+use InvalidArgumentException;
+
 class MandrillApi
 {
-    public $http;
-    public $key = null;
-    public $defaultOptions = [
-	    'merge_language'	=> 'handlebars',
-	    'inline_css'		=> true,
-    ];
+	/**
+	 * Holds the regex pattern for email validation
+	 *
+	 * @var string
+	 */
+	const EMAIL_PATTERN = '/^((?:[\p{L}0-9.!#$%&\'*+\/=?^_`{|}~-]+)*@[\p{L}0-9-.]+)$/ui';
+	/**
+	 * Regex for email validation
+	 *
+	 * If null, filter_var() will be used. Use the emailPattern() method
+	 * to set a custom pattern.'
+	 *
+	 * @var string
+	 */
+	protected $_emailPattern = self::EMAIL_PATTERN;
+
+
+	public $http;
+	public $config = null;
+	public $defaultConfig = [
+		'merge_language'	=> 'handlebars',
+		'inline_css'		=> true,
+		'global_merge_vars'	=> [ ],
+	];
+
+	public function __construct($config=[]) {
+		$this->config = Hash::merge($this->defaultConfig,Configure::read('Mandrill.default'),$config);
+	}
+
+	public function subject($subject) {
+		$this->config['subject'] = $subject;
+		return $this;
+	}
+
+	public function to($email) {
+		if (is_string($email)) {
+			$this->_addTo($email);
+		}
+		if (is_array($email)) {
+			foreach ($email as $key => $value) {
+				if (is_int($key)) {
+					$this->_validateEmail($value);
+					$this->_addTo($value);
+				} else {
+					$this->_validateEmail($key);
+					$this->_addTo($key,$value);
+				}
+			}
+		}
+		return $this;
+	}
+
+	public function from($email,$name=false) {
+		$this->config['from_email'] = $email;
+		if ($name!==false) {
+			$this->config['from_name'] = $name;
+		}
+		return $this;
+	}
+
+	public function send() {
+		debug($this->config);die;
+	}
+
+	protected function _addTo($email,$name=false) {
+		$_email = [
+			'email'	=>$email,
+			'type'	=>'to'
+		];
+		if ($name!==false) {
+			$_email['name'] = $name;
+		}
+		$this->config['to'][] = $_email;
+	}
+
+	/**
+	 * Validate email address
+	 *
+	 * @param string $email Email address to validate
+	 * @return void
+	 * @throws \InvalidArgumentException If email address does not validate
+	 */
+	protected function _validateEmail($email)
+	{
+		if ($this->_emailPattern === null) {
+			if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+				return;
+			}
+		} elseif (preg_match($this->_emailPattern, $email)) {
+			return;
+		}
+		throw new InvalidArgumentException(sprintf('Invalid email: "%s"', $email));
+	}
 
 }
 
